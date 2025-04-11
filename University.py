eb999e03ac194cbcbb4da6b7832a31f8597d340d
Youssef Fahmy <youssefhosny75@gmail.com>
2025-04-11 14:54:31 +0200

    Update University.py

--- University.py
+++ University.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import statsmodels
 
 st.set_page_config(layout="wide", page_title="📊 University Dashboard")
 
